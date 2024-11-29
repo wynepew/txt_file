0cc5763a0e8bcc5c11f1de82a1150a2940ee0234
wynepew <julianwyne12@gmail.com>
2024-11-30 01:01:59 +0800

    done with the file write, next is to ask user if need more person then try to see if working

--- txt_personal_info.py
+++ txt_personal_info.py
@@ -4,4 +4,10 @@ with open("./collected_info.txt", "a") as file:
         age = input("Enter your age: ")
         birthday = input("Enter your birthday: ")
         email_address = input("Enter your email: ")
-        phone_number = ("Enter your number: ")
+        phone_number = input("Enter your number: ")
+
+        file.write(f"Full Name: {full_name}")
+        file.write(f"Age: {age}")
+        file.write(f"Birthday: {birthday}")  
+        file.write(f"Email Address: {email_address}")
+        file.write(f"Phone Number: {phone_number}")
