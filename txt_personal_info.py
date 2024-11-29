762995c82c7d0b25e9a523434c2d224613a75007
wynepew <julianwyne12@gmail.com>
2024-11-30 00:56:26 +0800

    done with the user input

--- txt_personal_info.py
+++ txt_personal_info.py
@@ -1,2 +1,7 @@
-with open("./collected_info.txt", "a") as file_handle:
-    file_handle.write("okay start")
+with open("./collected_info.txt", "a") as file:
+    while True:
+        full_name = input("Enter your full name: ")
+        age = input("Enter your age: ")
+        birthday = input("Enter your birthday: ")
+        email_address = input("Enter your email: ")
+        phone_number = ("Enter your number: ")
